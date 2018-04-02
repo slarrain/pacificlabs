0a10c1d270c2f0b3845e109d1484ed34db56520a
Minghua Sun <clairesunstudio@gmail.com>
2018-04-01 21:19:03 -0400

    speed up carousel transition

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -331,10 +331,10 @@ if (typeof jQuery === 'undefined') {
 
   Carousel.VERSION  = '3.3.7'
 
-  Carousel.TRANSITION_DURATION = 600
+  Carousel.TRANSITION_DURATION = 200
 
   Carousel.DEFAULTS = {
-    interval: 5000,
+    interval: 6000,
     pause: 'hover',
     wrap: true,
     keyboard: true
